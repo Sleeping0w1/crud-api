d1aa431b86e9e36119cf7b4f4a39f9ad70a15bc9
Sleeping0w1 <1033351@gmail.com>
2025-11-08 09:50:57 +0300

    init: start crud-api task

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const fs = require('fs');
+
+const nodeModules = {};
+fs.readdirSync(path.resolve(__dirname, 'node_modules'))
+  .filter((x) => ['.bin'].indexOf(x) === -1)
+  .forEach((mod) => {
+    nodeModules[mod] = `commonjs ${mod}`;
+  });
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  mode: isProduction ? 'production' : 'development',
+  devtool: isProduction ? 'source-map' : 'eval-source-map',
+  entry: './src/server.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'server.js',
+  },
+  externals: nodeModules,
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+};
