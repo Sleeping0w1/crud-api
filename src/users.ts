a2bc4159b5eaba5631880846aa6a25c9225de8a8
Sleeping0w1 <1033351@gmail.com>
2025-11-08 10:51:04 +0300

    feat: implement core domain models for user CRUD

---
+++ src/users.ts
@@ -0,0 +1,49 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export class User {
+  public id = uuidv4();
+  constructor(
+    public username: string,
+    public age: number,
+    public hobbies: Array<string>
+  ) {}
+}
+
+export class Users {
+  private static _users: Map<string, User> = new Map();
+  constructor(startUsers?: Array<User>) {
+    if (Users._users.size === 0 && startUsers) {
+      startUsers.forEach(user => {
+        Users.addUser(user);
+      });
+    }
+  }
+  public static addUser(user: User) {
+    if (this._users.has(user.id)) {
+      throw new Error(`User with ID ${user.id} already exists`);
+    }
+    this._users.set(user.id, user);
+  }
+  public static updateUser(user: User) {
+    if (!this._users.has(user.id)) {
+      throw new Error(`User with ID ${user.id} not found`);
+    }
+    this._users.set(user.id, user);
+    return user;
+  }
+  public static removeUser(id: string): boolean {
+    if (!this._users.has(id)) {
+      throw new Error(`User with ID ${id} not found`);
+    }
+    return this._users.delete(id);
+  }
+  public static getUsers(): Array<User> {
+    return Array.from(this._users.values());
+  }
+  public static getUserById(id: string): User | undefined {
+    if (!this._users.has(id)) {
+      throw new Error(`User with ID ${id} not found`);
+    }
+    return this._users.get(id);
+  }
+}
