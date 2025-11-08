c861c248196deddffb0411a0c719ae21f1ff3a19
Sleeping0w1 <1033351@gmail.com>
2025-11-08 10:20:21 +0300

    feat: make server port configurable via .env

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import http from 'node:http';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'localhost';
 
 const server = http.createServer(function (request, response) {
   response.end('Hello!');
 });
-server.listen(3000, function () {
-  console.log('Сервер запущен по адресу http://localhost:3000');
+server.listen(PORT, function () {
+  console.log(`Сервер запущен по адресу http://${HOST}:${PORT}`);
 });
