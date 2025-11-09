444f1a632373fa5a9f1d3f3000b1ba4357194153
Sleeping0w1 <96081801+Sleeping0w1@users.noreply.github.com>
2025-11-09 21:57:10 +0300

    feat: add GET /users/{userId} endpoint

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const HTTP_STATUS_OK = 200;
 export const HTTP_STATUS_NOT_FOUND = 404;
+export const HTTP_BAD_REQUEST = 400;
 export const basicPort = 3000;
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
 import http from 'node:http';
 import dotenv from 'dotenv';
-import { Users } from './users.ts';
-import { basicPort, HTTP_STATUS_NOT_FOUND, HTTP_STATUS_OK } from './constants.ts';
+import { validate as validateUUID } from 'uuid';
+import {  Users } from './users.ts';
+import { basicPort, HTTP_BAD_REQUEST, HTTP_STATUS_NOT_FOUND, HTTP_STATUS_OK } from './constants.ts';
 
 dotenv.config();
-
 
 const PORT = process.env.PORT || basicPort;
 const HOST = process.env.HOST || 'localhost';
 
-Users.init();
+await Users.initializeFromJSON('./src/users.json');
 
 const server = http.createServer(function (request, response) {
   const { method, url } = request;
@@ -17,19 +17,42 @@ const server = http.createServer(function (request, response) {
     const allUsers = Users.getUsers();
     response.writeHead(HTTP_STATUS_OK, { 'Content-Type': 'application/json' });
     response.end(JSON.stringify(allUsers));
+  } else if (url?.startsWith('/users/') && method === 'GET') {
+    const id = url.split('/')[2];
+    if (!validateUUID(id)) {
+      response.writeHead(HTTP_BAD_REQUEST, { 'Content-Type': 'application/json' });
+      response.end(
+        JSON.stringify({
+          error: 'Invalid userId format',
+          message: 'UserId must be a valid UUID',
+        })
+      );
+      return;
+    }
+    const user = Users.getUserById(id);
+    if (user) {
+      response.writeHead(HTTP_STATUS_OK, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify(user));
+    } else {
+      response.writeHead(HTTP_STATUS_NOT_FOUND, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify({ error: 'Invalid userId', message: `record with id = ${id} doesn't exist` }));
+    }
+    return;
   } else {
     response.writeHead(HTTP_STATUS_NOT_FOUND, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify({
-      error: 'Page not found',
-      message: `Route ${method} ${url} not found`,
-      availableRoutes: [
-        'GET /users',
-        'GET /users/{userId}',
-        'POST /users',
-        'PUT /users/{userId}',
-        'DELETE /users/{userId}'
-      ]
-    }));
+    response.end(
+      JSON.stringify({
+        error: 'Page not found',
+        message: `Route ${method} ${url} not found`,
+        availableRoutes: [
+          'GET /users',
+          'GET /users/{userId}',
+          'POST /users',
+          'PUT /users/{userId}',
+          'DELETE /users/{userId}',
+        ],
+      })
+    );
   }
 });
 server.listen(PORT, function () {
