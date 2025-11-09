5fd7a8daa2415281508d075e322e977cc0fe79af
Sleeping0w1 <96081801+Sleeping0w1@users.noreply.github.com>
2025-11-09 21:28:02 +0300

    fix: error on compilation

--- src/users.ts
+++ src/users.ts
@@ -1,12 +1,17 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export class User {
-  public id = uuidv4();
-  constructor(
-    public username: string,
-    public age: number,
-    public hobbies: Array<string>
-  ) {}
+  public id: string;
+  public username: string;
+  public age: number;
+  public hobbies: string[];
+
+  constructor(username: string, age: number, hobbies: Array<string> = []) {
+    this.id = uuidv4();
+    this.username = username;
+    this.age = age;
+    this.hobbies = hobbies;
+  }
 }
 
 export class Users {
